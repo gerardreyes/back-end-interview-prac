9492531862916b353a3ea3d200c05313ddbe0967
Tim Chandler <tim@foodbyus.com>
2022-07-07 21:34:05 +1000

    Added the test code

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => redirect('/products'));
+Route::get('/products', 'App\Http\Controllers\ProductController@index');
+Route::post('/products/new', 'App\Http\Controllers\ProductController@new');
+Route::post('/products/delete', 'App\Http\Controllers\ProductController@delete');
